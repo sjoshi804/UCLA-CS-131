d1edd4fbdf2a16f5ee84e01a1c5938cff9dccd90
Siddharth Joshi <sjoshi804@gmail.com>
2019-04-11 00:00:31 -0700

    Created and tested convert_grammar hw1 -> hw2

---
+++ hw2/hw2.ml
@@ -0,0 +1,21 @@
+(* Convert hw1 grammar to hw2 grammar 
+A grammar in hw1 was a tuple of a non-terminal and list of tuples of (non terminals and a list of non terminals and terminals) 
+A grammar in hw 2 is a tuple of a non-terminal - the starting sumbol and a function that matches non-terminals to a list of possible symbols (symbols captured in lists) it can be used to represent
+*)
+(* Helper function that concatenates the symbols corresponding to a certain production*)
+let rec concatenateSnds = function
+  | [] -> []
+  | head::tail -> snd head::concatenateSnds tail
+
+(* Function that reduces multiple rules with same L.H.S. to one rule *)
+let rec mergeRules = function 
+  | [] -> []
+  | head::tail -> let fullList = head::tail in [((fst head), concatenateSnds (List.filter(fun x -> fst x = fst head) fullList))] @ mergeRules (List.filter(fun x -> not (fst x = fst head)) tail)
+
+(* helper function that flips arguments List.assoc - converting from association list to a function *)
+let reverseAssociate list key = List.assoc key list
+
+(* Function that applies the flipped List.assoc on a list where each L.H.S. appears only one and all the lists it used to match to are now elements in a superlist, returns a starting symbol
+i.e. grammar in hw2 - by taking as input grammar of hw1 *)
+let convert_grammar = function
+  | (startingSymbol, rules) -> (startingSymbol, reverseAssociate (mergeRules rules))
