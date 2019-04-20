95b9370e49cfdf450003d2f2d4bd3adf6591f372
Siddharth Joshi <sjoshi804@gmail.com>
2019-04-20 11:23:09 -0700

    manually put in List.tl to make it work

--- hw2/hw2.ml
+++ hw2/hw2.ml
@@ -70,17 +70,17 @@ let rec extract_rule = function
 | [] ->[]
 | hd::tail -> [extract_symbol hd] @ extract_rule tail 
 
-let rec first_new_rule current_rule = function
-| [] -> None
-| hd::tail -> if hd != current_rule then Some (hd) else first_new_rule current_rule tail 
+let rec only_new_rules current_rule = function
+| [] -> []
+| hd::tail -> if hd = current_rule then only_new_rules current_rule tail else hd::tail
 
 let rec try_next_rule func current_rule = function 
 | [] -> None
-| hd::tail -> 
-if hd = current_rule then 
-  let next = first_new_rule current_rule tail in 
-  if next = None then None else func (unwrap_list next)
-else try_next_rule func current_rule tail  
+| [a; b] -> if current_rule = a then func b else None
+| _ -> None
+
+let get2nd = function 
+| [a; b] -> b
 
 (* Actual make_matcher creates the actual matcher function that takes a fragment and 
 an acceptor and returns a tuple containing Some parse tree or None and Some suffix string or None*)
@@ -98,18 +98,23 @@ let actual_make_matcher = function
   in 
   if List.exists(fun x -> x = None) children then None else Some (List.map (fun x -> unwrap_tree x) (children))
   in
-let actual_matcher fragment acceptor = 
+let actual_matcher acceptor fragment = 
   let rec next_tree suffix = function 
   | Leaf a -> (None, a::suffix)
-  | Node (internal_node, children) -> 
-  let child_output = (next_tree_from_children suffix (List.rev children)) in
+  | Node (internal_node, children) -> let temp = (any_valid (rules_matcher suffix) (List.tl (rules internal_node))) in 
+  if temp = None then (None, suffix) else (Some (Node (internal_node, (unwrap_list temp))), suffix)(*
+  let new_children = rules_matcher suffix (get2nd (rules internal_node)) in
+   if unwrap_list new_children = children then (None, []) else *)(*
+  let new_rules_children = rules_matcher suffix [(*try_next_rule (rules_matcher suffix) (extract_rule children) (rules internal_node)*) in
+    if new_rules_children = None then (None, suffix) else (Some(Node(internal_node, children)), suffix)*)
+  (*let child_output = (next_tree_from_children suffix (List.rev children)) in
   let new_children = fst child_output in
   let new_suffix = snd child_output in 
   if new_children = None
   then 
-    let new_rules_children = try_next_rule (rules_matcher fragment) (extract_rule children) (rules internal_node) in
-    if new_rules_children = None then (None, new_suffix) else (Some(Node(internal_node, unwrap_list new_rules_children)), new_suffix)
-  else (Some(Node(internal_node, List.rev (unwrap_list new_children))), new_suffix)
+    let new_rules_children = try_next_rule (rules_matcher suffix) (extract_rule children) (rules internal_node) in
+    if new_rules_children = None then (None, new_suffix) else (Some(Node(internal_node, (unwrap_list new_rules_children))), new_suffix)
+  else (Some(Node(internal_node, List.rev (unwrap_list new_children))), new_suffix) *)
   and 
   next_tree_from_children suffix = function 
   | [] -> (None, suffix)
@@ -122,12 +127,13 @@ let actual_matcher fragment acceptor =
     else (Some ((unwrap_tree (fst next_tree_head))::tail), suffix)
   in 
   let rec try_acceptor parse_tree = 
-    let suffix = (parse_tree_leaves (unwrap_tree parse_tree)) in
-    let output = acceptor (revised_remainder fragment suffix) in 
+    let prefix = (parse_tree_leaves (unwrap_tree parse_tree)) in
+    let suffix = (revised_remainder fragment prefix) in
+    let output = acceptor suffix in 
     if output = None then 
-      let n_tree = fst (next_tree suffix (unwrap_tree parse_tree)) in 
+      let n_tree = fst (next_tree fragment (unwrap_tree parse_tree)) in 
       if n_tree = None then None 
-      else try_acceptor n_tree
+      else Some (n_tree, output)
     else Some (parse_tree, output)
   in 
   let start_tree = symbol_matcher fragment (N start_symbol)  in 
@@ -140,7 +146,7 @@ let unwrap_tuple = function
 
 (* Returns a function with a wrapper around the actual_matcher that returns what the acceptor returns*)
 let make_matcher grammar = 
-fun fragment acceptor -> let ans = ((actual_make_matcher grammar) fragment acceptor) in 
+fun acceptor fragment -> let ans = ((actual_make_matcher grammar) acceptor fragment) in 
 if ans = None then None
 else snd (unwrap_tuple ans)
 
@@ -149,35 +155,103 @@ let make_parser grammar =
   let accept_empty = function
   | [] -> Some []
   | _ -> None in 
-  fun fragment -> let ans = ((actual_make_matcher grammar) fragment accept_empty) in 
+  fun fragment -> let ans = ((actual_make_matcher grammar) accept_empty fragment) in 
   if ans = None then None
   else fst (unwrap_tuple ans)
-  
-  type awksub_nonterminals =
-    | Expr | Term | Lvalue | Incrop | Binop | Num
-  
-  let awkish_grammar =
-    (Expr,
-     function
-       | Expr ->
-           [[N Term; N Binop; N Expr];
-            [N Term]]
-       | Term ->
-     [[N Num];
-      [N Lvalue];
-      [N Incrop; N Lvalue];
-      [N Lvalue; N Incrop];
-      [T"("; N Expr; T")"]]
-       | Lvalue ->
-     [[T"$"; N Expr]]
-       | Incrop ->
-     [[T"++"];
-      [T"--"]]
-       | Binop ->
-     [[T"+"];
-      [T"-"]]
-       | Num ->
-     [[T"0"]; [T"1"]; [T"2"]; [T"3"]; [T"4"];
-      [T"5"]; [T"6"]; [T"7"]; [T"8"]; [T"9"]])
+
+  let accept_all string = Some string
+let accept_empty_suffix = function
+   | _::_ -> None
+   | x -> Some x
+
+(* An example grammar for a small subset of Awk.
+   This grammar is not the same as Homework 1; it is
+   instead the same as the grammar under
+   "Theoretical background" above.  *)
+
+type awksub_nonterminals =
+  | Expr | Term | Lvalue | Incrop | Binop | Num
+
+let awkish_grammar =
+  (Expr,
+   function
+     | Expr ->
+         [[N Term; N Binop; N Expr];
+          [N Term]]
+     | Term ->
+	 [[N Num];
+	  [N Lvalue];
+	  [N Incrop; N Lvalue];
+	  [N Lvalue; N Incrop];
+	  [T"("; N Expr; T")"]]
+     | Lvalue ->
+	 [[T"$"; N Expr]]
+     | Incrop ->
+	 [[T"++"];
+	  [T"--"]]
+     | Binop ->
+	 [[T"+"];
+	  [T"-"]]
+     | Num ->
+	 [[T"0"]; [T"1"]; [T"2"]; [T"3"]; [T"4"];
+    [T"5"]; [T"6"]; [T"7"]; [T"8"]; [T"9"]])
 
 let awk_parser = make_parser awkish_grammar
+
+let test0 =
+  ((make_matcher awkish_grammar) accept_all ["ouch"] = None)
+
+let test1 =
+  ((make_matcher awkish_grammar accept_all ["9"])
+   = Some [])
+
+let test2 =
+  ((make_matcher awkish_grammar accept_all ["9"; "+"; "$"; "1"; "+"])
+   = Some ["+"])
+
+let test3 =
+  ((make_matcher awkish_grammar accept_empty_suffix ["9"; "+"; "$"; "1"; "+"])
+   = None)
+
+(* This one might take a bit longer.... *)
+let test4 =
+ ((make_matcher awkish_grammar accept_all
+     ["("; "$"; "8"; ")"; "-"; "$"; "++"; "$"; "--"; "$"; "9"; "+";
+      "("; "$"; "++"; "$"; "2"; "+"; "("; "8"; ")"; "-"; "9"; ")";
+      "-"; "("; "$"; "$"; "$"; "$"; "$"; "++"; "$"; "$"; "5"; "++";
+      "++"; "--"; ")"; "-"; "++"; "$"; "$"; "("; "$"; "8"; "++"; ")";
+      "++"; "+"; "0"])
+  = Some [])
+
+let test5 =
+  (parse_tree_leaves (Node ("+", [Leaf 3; Node ("*", [Leaf 4; Leaf 5])]))
+   = [3; 4; 5])
+
+let small_awk_frag = ["$"; "1"; "++"; "-"; "2"]
+
+let test6 =
+  ((make_parser awkish_grammar small_awk_frag)
+   = Some (Node (Expr,
+		 [Node (Term,
+			[Node (Lvalue,
+			       [Leaf "$";
+				Node (Expr,
+				      [Node (Term,
+					     [Node (Num,
+						    [Leaf "1"])])])]);
+			 Node (Incrop, [Leaf "++"])]);
+		  Node (Binop,
+			[Leaf "-"]);
+		  Node (Expr,
+			[Node (Term,
+			       [Node (Num,
+				      [Leaf "2"])])])])))
+let test7 =
+  match make_parser awkish_grammar small_awk_frag with
+    | Some tree -> parse_tree_leaves tree = small_awk_frag
+    | _ -> false
+
+
+let test_grammar = [Expr, [T 0];Expr, [T 0; T 1]]
+let gram = convert_grammar (Expr, test_grammar)
+let p = make_parser gram
