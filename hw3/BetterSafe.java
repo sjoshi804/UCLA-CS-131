60741755e2cff96250d7fb3b9bfcc3412e8bdff1
Siddharth Joshi <sjoshi804@gmail.com>
2019-04-29 10:36:30 -0700

    Better safe implemented

---
+++ hw3/BetterSafe.java
@@ -0,0 +1,45 @@
+import java.util.concurrent.locks.ReentrantLock;
+
+class BetterSafe implements State {
+    private byte[] value;
+    private ReentrantLock value_lock;
+    private byte maxval;
+
+    BetterSafe(byte[] v) 
+    { 
+        value = v; 
+        maxval = 127; 
+        value_lock = new ReentrantLock();
+    }
+
+    BetterSafe(byte[] v, byte m) 
+    { 
+        value = v;
+        maxval = m; 
+        value_lock = new ReentrantLock();
+    }
+
+    public int size() 
+    {
+        return value.length;
+    }
+
+    public byte[] current() 
+    { 
+        return value; 
+    }
+
+    public boolean swap(int i, int j) 
+    {
+        value_lock.lock();
+        if (value[i] <= 0 || value[j] >= maxval) 
+        {
+            value_lock.unlock();
+            return false;
+        }
+        value[i]--;
+        value[j]++;
+        value_lock.unlock();
+        return true;
+    }
+}
