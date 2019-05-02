469135ccabd84c06d77d192a97a91f62026d5c58
Siddharth Joshi <sjoshi804@gmail.com>
2019-05-01 20:01:07 -0700

    Get N Set working

--- hw3/GetNSet.java
+++ hw3/GetNSet.java
@@ -1,4 +1,3 @@
-//Importing the class for Atomic Integer Array
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
 class GetNSet implements State {
@@ -6,37 +5,52 @@ class GetNSet implements State {
     private byte maxval;
 
     GetNSet(byte[] v) 
-    {
+	{
         maxval = 127;
-        value = new AtomicIntegerArray(v.length);
+        int[] int_array = new int[v.length];
+
         for (int i = 0; i < v.length; i++)
-            value.set(i, (int) v[i]);
+            int_array[i] = v[i];
+
+        value = new AtomicIntegerArray(int_array);
+        
     }
 
-    GetNSet(byte[] v, byte m)
-    {
+    GetNSet(byte[] v, byte m) 
+	{
         maxval = m;
-        for(int i = 0; i < v.length; i++)
-            value.addAndGet(i, (int) v[i]);
+        int[] int_array = new int[v.length];
+
+        for (int i = 0; i < v.length; i++)
+            int_array[i] = v[i];
+
+        value = new AtomicIntegerArray(int_array);
+        
     }
 
-    public int size() { return value.length(); }
+    public int size() 
+	{ 
+		return value.length(); 
+	}
 
     public byte[] current() 
-    {
-        byte[] result = new byte[value.length()];
-        for (int i = 0; i < value.length(); i++)
-            result[i] = (byte) value.get(i);
-        return result;
+	{
+        byte[] byte_array = new byte[value.length()];
+
+        for (int i = 0; i < value.length(); i++) 
+            byte_array[i] = (byte) value.get(i);
+
+        return byte_array;
     }
 
     public boolean swap(int i, int j) 
-    {
+	{
         int value_at_i = value.get(i);
         int value_at_j = value.get(j);
-        if (value_at_i <= 0 || value_at_j >= maxval) {
+
+        if (value_at_i <= 0 || value_at_j >= maxval) 
             return false;
-        }
+
         value.set(i, value_at_i - 1);
         value.set(j, value_at_j + 1);
         return true;
