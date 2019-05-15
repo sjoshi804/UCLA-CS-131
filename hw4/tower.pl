fc57dff21fc747a3cb7dc066ccc07df15cb2d296
Siddharth Joshi <sjoshi804@gmail.com>
2019-05-15 11:00:19 -0700

    plain tower almost done

--- hw4/tower.pl
+++ hw4/tower.pl
@@ -1,12 +1,13 @@
 % Defining the valid syntax for the counts data structure - depicting the visibile counts for the edges
 counts([], [], [], []).
 counts([_ | Top], [_ | Bottom], [_ | Left], [_ | Right]):- 
-    counts(Top, Bottom, Left, Right ). 
+    counts(Top, Bottom, Left, Right). 
 
 %Analog of List.hd from Ocaml
-hd([X | _], X).
-hd([_, Tl], X):-
-    hd(Tl, X).
+hd( [X | _], X).
+
+%Analog of List.tl from Ocaml
+tl( [_ | X], X).
 
 %Check length of all rows
 length_r([], _).
@@ -14,38 +15,65 @@ length_r([Row | Rest], N):-
     length(Row, N),
     length_r(Rest, N).
 
-%Checks if all elements 1 through N are members - i.e. checks for permutation
-perm(0, _).
-perm(N, Row):-
-    member(N, Row),
-    perm(N - 1, Row).
+%Create a list of elements from 1 through N
+new_list(0, []).
+new_list(N, [Hd | Tl]):-
+    length([Hd | Tl], N),
+    N = Hd,
+    N1 is N - 1,
+    new_list(N1, Tl).
 
-%Checks if Board has valid dimensions i.e N x N
-validDimensions(N, Board):-
-    length(Board, N),
-    length_r(Board, N).
+%Checks if all elements 1 through N are members - i.e. checks for permutation
+perm(N, Row):-
+    new_list(N, L),
+    permutation(L, Row).
 
 %Checks if all rows are valid
-validRows(N, []):-
-    number(N).
-validRows(N, [Row | Rest]):-
-    number(N), 
+valid_rows(_, []).
+valid_rows(N, [Row | Rest]):-
+    length(Row, N),
     perm(N, Row),
-    validRows(N, Rest).
+    valid_rows(N, Rest).
 
-%Transpose a matrix
-transpose([], []).
-transpose(Board, [])
-
-
+%Transpose a board
+transpose([[] | _], []).
+transpose(Board, [Col | Rest_Of_Cols]):-
+    maplist(hd, Board, Col),
+    maplist(tl, Board, Rest_Of_Rows),
+    transpose(Rest_Of_Rows, Rest_Of_Cols).
     
-validBoard(N, Board):-
-    validRows(N, Board),
+valid_board(N, Board):-
+    length(Board, N),
+    length_r(Board, N),
+    valid_rows(N, Board),
     transpose(Board, Board_T),
-    validRows(N, Board_T).
+    valid_rows(N, Board_T).
 
-%Plain tower - solves without finite domain solvering 
+numbers_in_range( _, []).
+numbers_in_range( N, [Hd | Tl]):-
+    Hd <= N,
+    numbers_in_range( N, Tl).
+
+valid_counts(N, counts(Top, Bottom, Left, Right)):-
+    numbers_in_range(N, Top),
+    numbers_in_range(N, Bottom),
+    numbers_in_range(N, Left),
+    numbers_in_range(N, Right).
+
+visibile()
+visibility( counts(Top, Bottom, Left, Right), Board):-
+    visible(Left, Board),
+    maplist(reverse, Board, Board_R),
+    visible(Right, Board_R),
+    transpose(Board, Board_T)
+    visible(Top, Board_T),
+    maplist(reverse, Board_T, Board_T_R)
+    visible(Bottom, Board_T_R).
+
+%Plain tower - solves without finite domain solver
 plain_tower(0, [], counts([], [], [], [])).
-plain_tower(N, Board, counts(Top, Bottom, Left, Right)):-
-    validDimensions(N, Board)
-    validBoard(N, Board)
+plain_tower(N, T, C):-
+    number(N),
+    valid_counts(N, C),
+    valid_board(N, T),
+    visibility(C, T).
