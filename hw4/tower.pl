13d0764a4686ddabe64bfc1624b9b90092a3a724
Siddharth Joshi <sjoshi804@gmail.com>
2019-05-16 00:30:24 -0700

    visibility works but not the whole thing, because its not glued together correctly

--- hw4/tower.pl
+++ hw4/tower.pl
@@ -1,4 +1,4 @@
-% Defining the valid syntax for the counts data structure - depicting the visibile counts for the edges
+% Defining the valid syntax for the counts data structure - depicting the visible counts for the edges
 counts([], [], [], []).
 counts([_ | Top], [_ | Bottom], [_ | Left], [_ | Right]):- 
     counts(Top, Bottom, Left, Right). 
@@ -9,31 +9,17 @@ hd( [X | _], X).
 %Analog of List.tl from Ocaml
 tl( [_ | X], X).
 
+%Permutation
+perm( 0, []).
+perm( N, Row):-
+    findall(X, between(1, N, X), Possible_Row),
+    permutation(Possible_Row, Row).
+
 %Check length of all rows
 length_r([], _).
 length_r([Row | Rest], N):-
     length(Row, N),
     length_r(Rest, N).
-
-%Create a list of elements from 1 through N
-new_list(0, []).
-new_list(N, [Hd | Tl]):-
-    length([Hd | Tl], N),
-    N = Hd,
-    N1 is N - 1,
-    new_list(N1, Tl).
-
-%Checks if all elements 1 through N are members - i.e. checks for permutation
-perm(N, Row):-
-    new_list(N, L),
-    permutation(L, Row).
-
-%Checks if all rows are valid
-valid_rows(_, []).
-valid_rows(N, [Row | Rest]):-
-    length(Row, N),
-    perm(N, Row),
-    valid_rows(N, Rest).
 
 %Transpose a board
 transpose([[] | _], []).
@@ -41,39 +27,43 @@ transpose(Board, [Col | Rest_Of_Cols]):-
     maplist(hd, Board, Col),
     maplist(tl, Board, Rest_Of_Rows),
     transpose(Rest_Of_Rows, Rest_Of_Cols).
-    
-valid_board(N, Board):-
-    length(Board, N),
-    length_r(Board, N),
-    valid_rows(N, Board),
-    transpose(Board, Board_T),
-    valid_rows(N, Board_T).
 
-numbers_in_range( _, []).
-numbers_in_range( N, [Hd | Tl]):-
-    Hd <= N,
-    numbers_in_range( N, Tl).
-
+%Checks if counts is valid
 valid_counts(N, counts(Top, Bottom, Left, Right)):-
-    numbers_in_range(N, Top),
-    numbers_in_range(N, Bottom),
-    numbers_in_range(N, Left),
-    numbers_in_range(N, Right).
+    maplist(between(1, N), Top),
+    maplist(between(1, N), Bottom),
+    maplist(between(1, N), Left),
+    maplist(between(1, N), Right).
 
-visibile()
-visibility( counts(Top, Bottom, Left, Right), Board):-
-    visible(Left, Board),
-    maplist(reverse, Board, Board_R),
-    visible(Right, Board_R),
-    transpose(Board, Board_T)
-    visible(Top, Board_T),
-    maplist(reverse, Board_T, Board_T_R)
-    visible(Bottom, Board_T_R).
+count_visible(_, [], 0).
+
+count_visible( Max_Yet, [Hd | Tl], Count):-
+    Max_Yet > Hd,
+    count_visible( Max_Yet, Tl, Count).
+
+count_visible( Max_Yet, [Hd | Tl], Count):-
+    Max_Yet < Hd,
+    count_visible(Hd, Tl, Count_minus_one),
+    Count is (Count_minus_one + 1).
+
+%Check visibility
+visibility( Board, Counts):-
+    maplist(count_visible(0), Board, Counts).
 
 %Plain tower - solves without finite domain solver
 plain_tower(0, [], counts([], [], [], [])).
 plain_tower(N, T, C):-
     number(N),
     valid_counts(N, C),
-    valid_board(N, T),
-    visibility(C, T).
+    length(Board, N),
+    maplist(perm(N), Board),
+    visibility(Board, Left),
+    maplist(reverse, Board, Board_R),
+    visibility(Board_R, Right),
+    transpose(Board, Board_T),
+    visibility(Board_T, Top),
+    maplist(reverse, Board_T, Board_T_R),
+    visibility(Board_T_R, Bottom),
+    transpose(Board, Board_T),
+    maplist(perm(N), Board_T).
+
