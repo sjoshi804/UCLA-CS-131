1a1ad2715a635297419e0b00a95cbb8ee11a7374
Siddharth Joshi <sjoshi804@gmail.com>
2019-05-16 13:34:21 -0700

    done w blazing fast plain_tower

--- hw4/tower.pl
+++ hw4/tower.pl
@@ -9,8 +9,10 @@ hd( [X | _], X).
 %Analog of List.tl from Ocaml
 tl( [_ | X], X).
 
-%Permutation
+%Permutation, checks if Row is permutation of 1...N
+%Base Case
 perm( 0, []).
+%between(1, N, X) -> X is a list with all elements from 1 ... N and tries to find all in Possible_Row
 perm( N, Row):-
     findall(X, between(1, N, X), Possible_Row),
     permutation(Possible_Row, Row).
@@ -22,29 +24,31 @@ length_r([Row | Rest], N):-
     length_r(Rest, N).
 
 %Transpose a board
+%Base case
 transpose([[] | _], []).
+
+%Recurse, First extract first column, then extract remaining rows and recurse
 transpose(Board, [Col | Rest_Of_Cols]):-
     maplist(hd, Board, Col),
     maplist(tl, Board, Rest_Of_Rows),
     transpose(Rest_Of_Rows, Rest_Of_Cols).
 
-%Checks if counts is valid
-valid_counts(N, counts(Top, Bottom, Left, Right)):-
-    maplist(between(1, N), Top),
-    maplist(between(1, N), Bottom),
-    maplist(between(1, N), Left),
-    maplist(between(1, N), Right).
-
+%Counts the number of towers visible in a row looking from left to right
+%Base case
 count_visible(_, [], 0).
 
+%First element is not visible -> Don't increment, Don't change Max_Yet
 count_visible( Max_Yet, [Hd | Tl], Count):-
     Max_Yet > Hd,
     count_visible( Max_Yet, Tl, Count).
 
+%First element is visible -> Increment and update Max_Yet
 count_visible( Max_Yet, [Hd | Tl], Count):-
     Max_Yet < Hd,
     count_visible(Hd, Tl, Count_minus_one),
-    Count is (Count_minus_one + 1).
+    Count is (Count_minus_one + 1). 
+    %Note Increment is done after recursion so that the binding given in the base case, cascades up 
+    %and is used. Wouldn't work in different order, despite commutativity of and. 
 
 %Check visibility
 visibility( Board, Counts):-
@@ -64,15 +68,19 @@ plain_tower( N, T, C):-
     maplist(reverse, Board_T, Board_T_R),
     visibility(Board_T_R, Bottom).
 
-
+%Checks if all elements in a list are unique
 all_unique([]).
 all_unique([Hd | Tl]):-
     \+ (member(Hd, Tl)), 
     all_unique(Tl).
 
+%Generates valid boards by checking if rows are permutations and simultaneously check if the implied column has all unique and check if row is valid from LeftCount and Right Count
+%Running multiple checks helps speed up considerably
+%Base Case
 valid_board( N, Accumulator, [], [], []):-
     length(Accumulator, N).
 
+%Recurse
 valid_board( N, Accumulator, [Row | Rest_Of_Rows], [Left_hd | Left_tl], [Right_hd | Right_tl]):-
     length(Accumulator, N_minus_Rest),
     N_minus_Rest < N,
@@ -84,12 +92,14 @@ valid_board( N, Accumulator, [Row | Rest_Of_Rows], [Left_hd | Left_tl], [Right_h
     maplist(all_unique, M_T),
     valid_board(N, [Row | Accumulator], Rest_Of_Rows, Left_tl, Right_tl).
 
+%No-optimization for N < 3
 ambiguous( N, C, T1, T2):-
     N < 3,
     plain_tower(N, T1, C),
     plain_tower(N, T2, C),
     \+ (T1 = T2). 
 
+%Sped-up version using random guess
 ambiguous( N, C, T1, T2):-
     N >= 3,
     C = counts(_, [3| _], _, _),
@@ -97,6 +107,7 @@ ambiguous( N, C, T1, T2):-
     plain_tower(N, T2, C),
     \+ (T1 = T2).     
 
+%Brute force search all possible answers for correctness
 ambiguous( N, C, T1, T2):-
     N >= 3,
     plain_tower(N, T1, C),
